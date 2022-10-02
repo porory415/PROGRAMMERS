33136b0fc6b53f0ac42fd9cec70ff2ab993f392f
johyyoon <johyyoon@student.42seoul.kr>
2022-10-02 21:58:43 +0900

    Level 3 and 4 complete

--- MYSQL/Lv.3&4/입양 시간 구하기(2).sql
+++ MYSQL/Lv.3&4/입양 시간 구하기(2).sql
@@ -2,7 +2,8 @@
 -- 0시부터 23시까지, 각 시간대별로 입양이 몇 건이나 발생했는지 조회하는 SQL문을 작성해주세요.
 -- 이때 결과는 시간대 순으로 정렬해야 합니다.
 
-SET @hour := -1; -- 변수 선언
+SET @hour := -1;
+--변수 선언
 
 SELECT (@hour := @hour + 1) as HOUR,
     (select count(hour(datetime))
